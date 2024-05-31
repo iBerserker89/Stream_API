17b7ba731b98f13e1e893a7420c438a789cefe82
Luciano Barros <lucianof.barros89@gmail.com>
2024-05-31 18:55:08 -0300

    Enviando Predicate e melhoria em Consumer
    Signed-off-by: Luciano Barros <lucianof.barros89@gmail.com>

--- src/Main/Functional_Interface/exemplos/ConsumerExample.java
+++ src/Main/Functional_Interface/exemplos/ConsumerExample.java
@@ -17,18 +17,15 @@ public class ConsumerExample {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
         // Usar o Consumer com expressão lambda para imprimir números pares
-//        Consumer<Integer> imprimirNumeroPar = numero -> {
-//            if (numero % 2 == 0) {
-//                System.out.println(numero);
-//            }
-//        };
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
 
         // Usar o Consumer para imprimir números pares no Stream
-        numeros.forEach(n -> {
-                if (n % 2 == 0) {
-                    System.out.println(n);
-                }
-            }
-        );
+        numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
     }
 }
