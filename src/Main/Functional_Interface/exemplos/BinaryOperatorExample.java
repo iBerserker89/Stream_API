beeccce18521328273f7b77ad53e27ae10742cc2
Luciano Barros <lucianof.barros89@gmail.com>
2024-06-03 13:02:42 -0300

    Enviando method reference em BinaryOperator
    Signed-off-by: Luciano Barros <lucianof.barros89@gmail.com>

--- src/Main/Functional_Interface/exemplos/BinaryOperatorExample.java
+++ src/Main/Functional_Interface/exemplos/BinaryOperatorExample.java
@@ -13,14 +13,14 @@ public class BinaryOperatorExample {
 
     public static void main(String[] args) {
         // Criar uma lista de números inteiros
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6);
 
         // Usar o BinaryOperator com expressão lambda para somar dois números inteiros
-        BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+        BinaryOperator<Integer> somar = Integer::sum;
 
         // Usar o BinaryOperator para somar todos os números no Stream
         int resultado = numeros.stream()
-                .reduce(0, somar);
+                .reduce(0, Integer::sum);
 
         // Imprimir o resultado da soma
         System.out.println("A soma dos números é: " + resultado);
